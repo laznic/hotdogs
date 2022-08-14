86800eac3ea30bd17fcbd9cadd04341294baecb6
unknown <niklas@demtrees.com>
2022-08-14 12:34:41 +0300

    Add initial local video feed to be worked on

--- src/modules/game/components/VideoCard.tsx
+++ src/modules/game/components/VideoCard.tsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import tw from 'twin.macro'
+import { useRef } from 'react';
+import { useEffect } from 'react';
 
 export default function VideoCard() {
+  const videoElement = useRef(null)
+
+  useEffect(function loadVideo () {
+    async function load() {
+      const stream = await navigator.mediaDevices.getUserMedia({ video: {} })
+      videoElement.current.srcObject = stream
+    }
+
+    load()
+  }, [])
+
   return (
     <Card>
-      Video card
+      <video ref={videoElement} autoPlay muted playsInline />
     </Card>
   )
 }
