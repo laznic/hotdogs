b3a1468382e7dd1f855d624f4083d68ac973adb4
unknown <niklas@demtrees.com>
2022-08-14 15:59:10 +0300

    Add initial face recognition functionality to be worked on

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,8 @@
 import { useRoutes } from 'react-router-dom'
 import routes from './modules/navigation/routes'
-import * as faceapi from 'face-api.js';
-import { useEffect } from 'react';
-
-const MODEL_URL = '/models'
 
 function App() {
   const element = useRoutes(routes)
-
-  useEffect(() => {
-    async function loadModels () {
-      await faceapi.loadTinyFaceDetectorModel(MODEL_URL)
-      await faceapi.loadFaceLandmarkTinyModel(MODEL_URL)
-      await faceapi.loadFaceLandmarkModel(MODEL_URL)
-      await faceapi.loadFaceRecognitionModel(MODEL_URL)
-      await faceapi.loadFaceExpressionModel(MODEL_URL)
-      console.log(faceapi.nets)
-    }
-
-    loadModels()
-  }, [])
 
   return (
     <div>
--- src/modules/game/components/VideoCard.tsx
+++ src/modules/game/components/VideoCard.tsx
@@ -1,13 +1,46 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 import tw from 'twin.macro'
-import { useRef } from 'react';
-import { useEffect } from 'react';
+import * as faceapi from '@vladmandic/face-api';
+import takeLast from 'ramda/src/takeLast'
+
+const MODEL_URL = '/models'
 
 export default function VideoCard() {
   const videoElement = useRef(null)
+  const requestAnimationRef = useRef(null)
+
+  async function onPlay () {
+      const options = new faceapi.TinyFaceDetectorOptions({ inputSize: 512, scoreThreshold: 0.5 })
+      const result = await faceapi.detectSingleFace(videoElement.current, options).withFaceLandmarks(true)
+
+      if (result) {
+        const mouth = result.landmarks.getMouth()
+        const bottomLip = takeLast(3, mouth)
+        const upperLip = takeLast(3, mouth.slice(0, mouth.length - 4))
+  
+        const bottomLipPositionAverage = bottomLip.reduce(sumYPositions, 0) / bottomLip.length
+        const upperLipPositionAverage = upperLip.reduce(sumYPositions, 0) / upperLip.length
+  
+        const distanceBetweenLips = getDifference(upperLipPositionAverage, bottomLipPositionAverage)
+        
+        if (distanceBetweenLips >= 15) console.log('mouth open')
+        else console.log('mouth closed')
+      }
+
+  }
+
+  useAnimationFrame(() => onPlay())
+
+  async function loadModels () {
+    await faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL)
+    await faceapi.nets.faceLandmark68TinyNet.loadFromUri(MODEL_URL)
+    await faceapi.nets.faceRecognitionNet.loadFromUri(MODEL_URL)
+    await faceapi.nets.faceExpressionNet.loadFromUri(MODEL_URL)
+  }
 
   useEffect(function loadVideo () {
     async function load() {
+      loadModels()
       const stream = await navigator.mediaDevices.getUserMedia({ video: {} })
       videoElement.current.srcObject = stream
     }
@@ -15,11 +48,44 @@ export default function VideoCard() {
     load()
   }, [])
 
+
   return (
     <Card>
       <video ref={videoElement} autoPlay muted playsInline />
     </Card>
   )
+}
+
+const useAnimationFrame = callback => {
+  // Use useRef for mutable variables that we want to persist
+  // without triggering a re-render on their change
+  const requestRef = React.useRef();
+  const previousTimeRef = React.useRef();
+  /**
+   * The callback function is automatically passed a timestamp indicating
+   * the precise time requestAnimationFrame() was called.
+   */
+
+  React.useEffect(() => {
+    const animate = time => {
+      if (previousTimeRef.current !== undefined) {
+        const deltaTime = time - previousTimeRef.current;
+        callback(deltaTime);
+      }
+      previousTimeRef.current = time;
+      requestRef.current = requestAnimationFrame(animate);
+    };
+    requestRef.current = requestAnimationFrame(animate);
+    return () => cancelAnimationFrame(requestRef.current);
+  }, []); // Make sure the effect runs only once
+};
+
+function sumYPositions(acc: number, curr: Record<string, number>) {
+  return curr.y + acc
+}
+
+function getDifference(a: number, b: number) {
+  return Math.abs(a - b);
 }
 
 const Card = tw.div`
