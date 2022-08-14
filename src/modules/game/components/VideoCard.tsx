a8d18f4f8423238004dd84083ec25c6ae134167b
unknown <niklas@demtrees.com>
2022-08-14 17:01:27 +0300

    Add initial hot dog eating functionality

--- src/modules/game/components/VideoCard.tsx
+++ src/modules/game/components/VideoCard.tsx
@@ -1,13 +1,18 @@
-import React, { useRef, useEffect } from 'react'
+import React, { useRef, useEffect, useState } from 'react'
 import tw from 'twin.macro'
 import * as faceapi from '@vladmandic/face-api';
 import takeLast from 'ramda/src/takeLast'
+import update from 'ramda/src/update'
+import append from 'ramda/src/append'
 
 const MODEL_URL = '/models'
 
 export default function VideoCard() {
   const videoElement = useRef(null)
-  const requestAnimationRef = useRef(null)
+  const mouthState = useRef(null)
+  const hotDogBase = { bites: 0, finished: false }
+  const [hotDogs, setHotDogs] = useState([hotDogBase])
+  const currentDogIndex = useRef(0)
 
   async function onPlay () {
       const options = new faceapi.TinyFaceDetectorOptions({ inputSize: 512, scoreThreshold: 0.5 })
@@ -20,16 +25,30 @@ export default function VideoCard() {
   
         const bottomLipPositionAverage = bottomLip.reduce(sumYPositions, 0) / bottomLip.length
         const upperLipPositionAverage = upperLip.reduce(sumYPositions, 0) / upperLip.length
-  
+
         const distanceBetweenLips = getDifference(upperLipPositionAverage, bottomLipPositionAverage)
         
-        if (distanceBetweenLips >= 15) console.log('mouth open')
-        else console.log('mouth closed')
-      }
+        if (distanceBetweenLips <= 55 && distanceBetweenLips >= 12) {
+          mouthState.current = 'open'
+        } else {
+          if (mouthState.current === 'open') {
+            const bites = hotDogs[currentDogIndex.current].bites + 1
+            const finished = bites === 3
+            let updatedDogs = update(currentDogIndex.current, { bites, finished }, hotDogs)
 
+            if (finished) {
+              updatedDogs = append(hotDogBase, updatedDogs)
+              currentDogIndex.current = currentDogIndex.current + 1
+            }
+
+            setHotDogs(updatedDogs)
+            mouthState.current = 'closed'
+          }
+        }
+      }
   }
 
-  useAnimationFrame(() => onPlay())
+  useAnimationFrame(() => onPlay(), hotDogs)
 
   async function loadModels () {
     await faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL)
@@ -48,40 +67,36 @@ export default function VideoCard() {
     load()
   }, [])
 
-
   return (
     <Card>
       <video ref={videoElement} autoPlay muted playsInline />
+      {hotDogs.map((hotDog: Record<string, unknown>, index: number) => <p key={index}>bites: {hotDog.bites} - finished: {hotDog.finished ? 'true' : 'false'}</p>)}
     </Card>
   )
 }
 
-const useAnimationFrame = callback => {
+const useAnimationFrame = (callback: () => void, deps: unknown) => {
   // Use useRef for mutable variables that we want to persist
   // without triggering a re-render on their change
-  const requestRef = React.useRef();
-  const previousTimeRef = React.useRef();
+  const requestRef = React.useRef()
   /**
    * The callback function is automatically passed a timestamp indicating
    * the precise time requestAnimationFrame() was called.
    */
 
   React.useEffect(() => {
-    const animate = time => {
-      if (previousTimeRef.current !== undefined) {
-        const deltaTime = time - previousTimeRef.current;
-        callback(deltaTime);
-      }
-      previousTimeRef.current = time;
-      requestRef.current = requestAnimationFrame(animate);
-    };
-    requestRef.current = requestAnimationFrame(animate);
-    return () => cancelAnimationFrame(requestRef.current);
-  }, []); // Make sure the effect runs only once
-};
+    const animate = () => {
+      callback()
+      requestRef.current = requestAnimationFrame(animate)
+    }
+  
+    requestRef.current = requestAnimationFrame(animate)
+    return () => cancelAnimationFrame(requestRef.current)
+  }, [deps]) // Make sure the effect runs only once
+}
 
 function sumYPositions(acc: number, curr: Record<string, number>) {
-  return curr.y + acc
+  return curr._y + acc
 }
 
 function getDifference(a: number, b: number) {
