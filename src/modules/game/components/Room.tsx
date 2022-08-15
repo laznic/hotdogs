180698b6c1b016d1f2155c9dc22d48ce3af90a9c
unknown <niklas@demtrees.com>
2022-08-15 15:34:27 +0300

    Add initial game joining functionality

--- src/modules/game/components/Room.tsx
+++ src/modules/game/components/Room.tsx
@@ -1,7 +1,31 @@
 import React from 'react'
+import { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { useSupabase } from '../../../contexts/SupabaseContext';
 import VideoCard from "./VideoCard";
 
-export default function Room() {
+export default function Room({ viewMode }: { viewMode?: boolean }) {
+  const params = useParams()
+
+  const { rpcQuery } = useSupabase()
+
+  async function joinGame () {
+    console.log(params)
+
+    const { data, error } = await rpcQuery('join_game', { id: params?.id })
+    if (error) {
+      console.log(error)
+    }
+
+    console.log(data)
+  } 
+
+  useEffect(() => {
+    if (params?.code) {
+      joinGame()
+    }
+  }, [params])
+
   return (
     <>
       <p>room</p>
