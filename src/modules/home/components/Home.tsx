421a0e97921b354160526ba1cfd6a1ff7372d706
unknown <niklas@demtrees.com>
2022-08-15 15:36:00 +0300

    Add initial game creation and logout functionalities to be worked on

--- src/modules/home/components/Home.tsx
+++ src/modules/home/components/Home.tsx
@@ -1,30 +1,40 @@
-import { Session } from '@supabase/supabase-js'
-import React, { useState, useEffect } from 'react'
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import { useSupabase } from '../../../contexts/SupabaseContext'
 
 export default function Home() {
-  const [session, setSession] = useState<Session | null>(null)
-
-  const { client } = useSupabase()
+  const { client, session } = useSupabase()
+  const navigate = useNavigate()
 
   async function handleLogin () {
-
     await client.auth.signIn({
         provider: 'twitter'
      })
   }
 
-  useEffect(() => {
-    setSession(client.auth.session())
+  async function handleLogout () {
+    await client.auth.signOut()
+  }
 
-    client.auth.onAuthStateChange((_event, session) => {
-      setSession(session)
+  async function createRoom () {
+    const { data, error } = await client.from('games').insert({
+      code: 'asdlol123',
+      private: false
     })
-  }, [])
+
+    if (error) {
+      console.log(error)
+      return
+    }
+
+    const [game] = data
+    navigate(`/rooms/${game.id}/${game.code}`)
+  }
 
   return (
     <>
-      <p>{session ? `Welcome ${session.user?.user_metadata.preferred_username}` : <button onClick={handleLogin}>Login</button>}</p>
+      <p>{session ? <>{`Welcome ${session.user?.user_metadata.preferred_username}`}<button onClick={handleLogout}>Logout</button></> : <button onClick={handleLogin}>Login</button>}</p>
+      {session && <button onClick={createRoom}>Create game</button>}
       <section className="grid grid-cols-4">
       </section>
     </>
