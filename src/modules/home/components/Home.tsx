3a9ef5cd06ab8dff1df0f5aaf2fa59c05f8d5761
unknown <niklas@demtrees.com>
2022-08-15 16:38:56 +0300

    Add some initial elements and data fetching to be worked on

--- src/modules/home/components/Home.tsx
+++ src/modules/home/components/Home.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useSupabase } from '../../../contexts/SupabaseContext'
+import LoginModal from '../../auth/components/LoginModal'
 
 export default function Home() {
   const { client, session } = useSupabase()
   const navigate = useNavigate()
+  const [publicGames, setPublicGames] = useState([])
 
   async function handleLogin () {
     await client.auth.signIn({
@@ -31,11 +33,52 @@ export default function Home() {
     navigate(`/rooms/${game.id}/${game.code}`)
   }
 
+  async function fetchPublicGames () {
+    const { data, error } = await client.from('games').select(`
+      id,
+      status,
+      created_at,
+      viewer_count,
+      max_player_count,
+      players:games_players (
+        ready,
+        hotdogs
+      )
+    `)
+    .order('id', { ascending: false })
+    .neq('status', 'FINISHED')
+    .eq('private', false)
+    
+    if (error) {
+      console.log(error)
+      return
+    }
+
+    console.log(data)
+
+    setPublicGames(data)
+  }
+
+  useEffect(() => {
+    fetchPublicGames()
+  }, [])
+
   return (
     <>
       <p>{session ? <>{`Welcome ${session.user?.user_metadata.preferred_username}`}<button onClick={handleLogout}>Logout</button></> : <button onClick={handleLogin}>Login</button>}</p>
       {session && <button onClick={createRoom}>Create game</button>}
-      <section className="grid grid-cols-4">
+      {!session && <LoginModal />}
+      <section className="mt-8">
+        <h3 className="text-3xl font-bold text-center text-rose-900">Or join some of the public games 👇</h3>
+        <ul>
+          {publicGames.map((game) => {
+            return (
+              <li key={game.id}>
+                {game.status} | {game.players.length} / {game.max_player_count} players | {game.viewer_count} people viewing
+              </li>
+            )
+          })} 
+        </ul>
       </section>
     </>
   )
