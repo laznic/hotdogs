e07ee7d10ee16d9a54eef342b927ce0a8fe03cc5
unknown <niklas@demtrees.com>
2022-08-16 21:29:00 +0300

    Adjust styles even more

--- src/modules/game/components/OtherPlayerCard.tsx
+++ src/modules/game/components/OtherPlayerCard.tsx
@@ -1,32 +1,36 @@
 import React, { useRef, useEffect, useState } from 'react'
-import tw from 'twin.macro'
+import tw, { styled } from 'twin.macro'
 import FaceBlock from './FaceBlock';
 
 interface OtherPlayerCardProps {
-  hotDogs: { bites: number, finished: boolean }[]
+  hotDogs: { bites: number, finished: boolean }[],
+  ready: boolean
 }
 
-export default function OtherPlayerCard({ hotDogs }: OtherPlayerCardProps) {
+export default function OtherPlayerCard({ hotDogs, ready }: OtherPlayerCardProps) {
   return (
-    <Card>
+    <Card ready={ready}>
       <FaceBlock hotDogsEaten={hotDogs?.length || 1} emoji={''} />
     </Card>
   )
 }
 
-const Card = tw.div`
-  bg-white
-  border-neutral-200
-  rounded-md
-  border-b-2
-  border-r-2
-  p-4
-  overflow-hidden
-  shadow-2xl
-  even:rotate-6
-  odd:-rotate-2
-  sm:even:translate-y-8
-  sm:odd:-translate-y-4
-  h-fit
-  max-w-xs
-`
+const Card = styled.div(({ ready }: { ready: boolean }) => [
+  tw`
+    bg-white
+    border-neutral-200
+    rounded-md
+    border-b-2
+    border-r-2
+    p-4
+    overflow-hidden
+    shadow-2xl
+    even:rotate-6
+    odd:-rotate-2
+    sm:even:translate-y-8
+    sm:odd:-translate-y-4
+    h-fit
+    max-w-xs
+  `,
+  ready && tw`ring-8 ring-violet-500 border-transparent border-4 after:content-['Ready'] after:inline-flex after:text-violet-500 after:translate-y-2 text-center text-xl font-black`
+])
--- src/modules/game/components/Room.tsx
+++ src/modules/game/components/Room.tsx
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom';
 import update from 'ramda/src/update'
 import addSeconds from 'date-fns/addSeconds'
 import { useTimer } from 'react-timer-hook'
+import tw from 'twin.macro'
 import { useSupabase } from '../../../contexts/SupabaseContext';
 import MyPlayerCard from "./MyPlayerCard";
 import JoinLinkModal from './JoinLinkModal';
@@ -118,21 +119,62 @@ export default function Room({ viewMode }: { viewMode?: boolean }) {
 
   return (
     <>
-      <button onClick={toggleJoinLinkModal}>Show join code</button>
+      {showJoinLinkModal && <JoinLinkModal isOpen={showJoinLinkModal} toggleModal={toggleJoinLinkModal}  />}
       <section className="grid gap-12 items-center mx-auto justify-center">
-        {showJoinLinkModal && <JoinLinkModal isOpen={showJoinLinkModal} toggleModal={toggleJoinLinkModal}  />}
 
-        <div className="max-w-sm mx-auto">
-          <MyPlayerCard emoji={emoji} setEmoji={setEmoji} />
-          <span className="bg-violet-500 text-white font-extrabold w-full flex justify-center py-2 mt-4 rounded">You (@{session?.user?.user_metadata.preferred_username})</span>
+        <div className="max-w-sm mx-auto text-center">
+        <MyPlayerCard emoji={emoji} setEmoji={setEmoji} />
+          <span className="bg-sky-500 text-sky-50 font-extrabold w-full flex justify-center py-2 mt-4 rounded">You (@{session?.user?.user_metadata.preferred_username})</span>
+
+          <ShowJoinCodeButton onClick={toggleJoinLinkModal}>
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+              <path strokeLinecap="round" strokeLinejoin="round" d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1" />
+            </svg>
+            Show join code
+          </ShowJoinCodeButton>
+
+          <p className="text-rose-900 font-semibold text-center">
+            Check that your camera works by opening and closing your mouth. When everyone is ready,
+            <StartGameButton>start the game.</StartGameButton>
+          </p>
+
         </div>
 
         <div className="flex flex-wrap sm:flex-nowrap justify-evenly sm:w-1/2 xl:w-3/4 mx-auto">
+          {!participants.filter((participant) => !isMe(participant.user_id)).length && <span className="text-rose-700">Waiting for other players</span> }
           {participants.filter((participant) => !isMe(participant.user_id)).map((participant) => (
-            <OtherPlayerCard key={participant.id} hotDogs={participant.hotdogs} />
+            <OtherPlayerCard key={participant.id} hotDogs={participant.hotdogs} ready={participant.ready} />
           ))}
         </div>
       </section>
     </>
   )
 }
+
+const ShowJoinCodeButton = tw.button`
+  inline-flex
+  items-center
+  font-semibold
+  rounded-md
+  text-rose-800
+  hover:text-neutral-800
+  py-2
+  my-2
+`
+
+const StartGameButton = tw.button`
+  rounded
+  bg-violet-500
+  text-white
+  border
+  border-violet-600
+  px-4
+  py-2
+  mt-4
+  mb-6
+  font-bold
+  shadow-lg
+  hover:-translate-y-2
+  hover:shadow-xl
+  transition-all
+`
