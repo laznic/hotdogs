bd3ccb55c49a9cc0f97792c6704ea94c80bf1bb1
unknown <niklas@demtrees.com>
2022-08-17 15:59:17 +0300

    Add more functionality to start a game, and to handle player joining correctly
    Players without the code should be kicked out if it's not a public game

--- src/modules/game/components/Room.tsx
+++ src/modules/game/components/Room.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useRef } from 'react'
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import update from 'ramda/src/update'
 import addSeconds from 'date-fns/addSeconds'
 import { useTimer } from 'react-timer-hook'
@@ -9,23 +9,26 @@ import MyPlayerCard from "./MyPlayerCard";
 import JoinLinkModal from './JoinLinkModal';
 import OtherPlayerCard from './OtherPlayerCard';
 
-export default function Room({ viewMode }: { viewMode?: boolean }) {
+export default function Room() {
   const params = useParams()
   const { client, rpcQuery, session } = useSupabase()
   const [emoji, setEmoji] = useState('😊')
   const [participants, setParticipants] = useState([])
-  const [startingGame, setStartingGame] = useState(false)
   const [gameStatus, setGameStatus] = useState('OPEN')
   const [game, setGame] = useState()
-  const time = useRef(addSeconds(new Date(), 30))
-  const [showJoinLinkModal, setShowJoinLinkModal] = useState(true)
+  const [myPlayerId, setMyPlayerId] = useState()
+  const now = useRef(new Date)
+  const [showJoinLinkModal, setShowJoinLinkModal] = useState()
+  const gameStarted = gameStatus === 'IN_PROGRESS'
+  const { seconds, restart: restartTimer } = useTimer({ expiryTimestamp: now.current, autoStart: false })
+  const { seconds: countdown, restart: restartCountdown } = useTimer({ expiryTimestamp: now.current, autoStart: false })
+  const navigate = useNavigate()
   
-  function isMe (id: string) {
-    return id === session?.user.id
+  function isMe (userId: string, playerId: string) {
+    return userId === session?.user.id || playerId === myPlayerId
   }
 
   function handlePlayerJoin (payload: Record<string, unknown>) {
-    console.log(payload.new, participants)
     setParticipants(participants.concat([payload.new]))
   }
 
@@ -37,20 +40,18 @@ export default function Room({ viewMode }: { viewMode?: boolean }) {
   }
 
   function handleGameState (payload: Record<string, unknown>) {
+    if (payload.new.status === 'STARTING') {
+      const countdownSeconds = addSeconds(new Date(), 5)
+      restartCountdown(countdownSeconds, true)
+    }
+
+    if (payload.new.status === 'IN_PROGRESS') {
+      const gameSeconds = addSeconds(new Date(), 15)
+      restartTimer(gameSeconds, true)
+    }
+ 
     setGameStatus(payload.new.status)
   }
-
-  async function fetchParticipants() {
-    const { data, error } = await client.from('games_players')
-      .select()
-      .eq('game', params?.id)
-      
-    if (!error) {
-      setParticipants(data)
-    }
-  }
-
-  const { seconds } = useTimer({ expiryTimestamp: time.current, autoStart: false })
 
   async function endGame() {
     await rpcQuery('end_game', { id: params?.id })
@@ -66,13 +67,11 @@ export default function Room({ viewMode }: { viewMode?: boolean }) {
     await rpcQuery('start_game', { id: params?.id })
   }
 
-  
-  if (seconds === 0) {
-    return <p>Time is up!</p>
+  async function startGameCountdown () {
+    await rpcQuery('start_game_countdown', { id: params?.id })
   }
 
   useEffect(function removeSubscriptions () {
-    fetchParticipants()
     fetchGame()
 
     client.from(`games_players:game=eq.${params?.id}`)
@@ -92,58 +91,86 @@ export default function Room({ viewMode }: { viewMode?: boolean }) {
 
   async function joinGame () {
     const { data, error } = await rpcQuery('join_game', { id: params?.id })
+
     if (error) {
       console.log(error)
+      return
     }
 
-    console.log(data)
+    setMyPlayerId(data)
   } 
 
   useEffect(() => {
-    if (params?.code) {
+    if (game && (!game.private || params?.code) && !myPlayerId) {
       joinGame()
+    } else {
+      navigate('/', { replace: true })
     }
-  }, [params?.code])
+  }, [game, params?.code, myPlayerId])
+
+  useEffect(function startGameCountdown() {
+    if (countdown === 0 && gameStatus === 'STARTING') {
+      startGame()
+    }
+  }, [countdown, gameStatus])
 
   function toggleJoinLinkModal () {
     setShowJoinLinkModal(!showJoinLinkModal)
   }
 
-  async function fetchGame () {
+  async function  fetchGame () {
     const { data, error } = await rpcQuery('fetch_game', { id: params?.id })
 
     if (!error) {
+      const { participants } = data
       setGame(data)
+      setParticipants(participants)
     }
-  } 
+  }
+    
+  if (seconds === 0 && gameStarted) {
+    return <p>Time is up!</p>
+  }
+
+  const createdByMe = game?.created_by === session?.user.id
 
   return (
     <>
-      {showJoinLinkModal && <JoinLinkModal isOpen={showJoinLinkModal} toggleModal={toggleJoinLinkModal}  />}
+      {showJoinLinkModal && !gameStarted && <JoinLinkModal isOpen={showJoinLinkModal} toggleModal={toggleJoinLinkModal}  />}
       <section className="grid gap-12 items-center mx-auto justify-center">
 
         <div className="max-w-sm mx-auto text-center">
-        <MyPlayerCard emoji={emoji} setEmoji={setEmoji} />
+        <MyPlayerCard emoji={emoji} setEmoji={setEmoji} gameStarted={gameStarted} />
           <span className="bg-sky-500 text-sky-50 font-extrabold w-full flex justify-center py-2 mt-4 rounded">You (@{session?.user?.user_metadata.preferred_username})</span>
+          {!gameStarted && (
+            <>
+              {createdByMe && (
+                <ShowJoinCodeButton onClick={toggleJoinLinkModal}>
+                  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1" />
+                  </svg>
+                  Show join code
+                </ShowJoinCodeButton>
+              )}
 
-          <ShowJoinCodeButton onClick={toggleJoinLinkModal}>
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-              <path strokeLinecap="round" strokeLinejoin="round" d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1" />
-            </svg>
-            Show join code
-          </ShowJoinCodeButton>
-
-          <p className="text-rose-900 font-semibold text-center">
-            Check that your camera works by opening and closing your mouth. When everyone is ready,
-            <StartGameButton>start the game.</StartGameButton>
-          </p>
-
+              <p className="text-rose-900 font-semibold text-center mt-4">
+                Check that your camera works by opening and closing your mouth: the emoji should react when you do so.
+                {createdByMe && (
+                  <>
+                    When everyone is ready,
+                    <br />
+                    <StartGameButton onClick={startGameCountdown}>start the game.</StartGameButton>
+                  </>
+                )}
+              </p>
+            </>
+          )}
         </div>
 
         <div className="flex flex-wrap sm:flex-nowrap justify-evenly sm:w-1/2 xl:w-3/4 mx-auto">
-          {!participants.filter((participant) => !isMe(participant.user_id)).length && <span className="text-rose-700">Waiting for other players</span> }
-          {participants.filter((participant) => !isMe(participant.user_id)).map((participant) => (
-            <OtherPlayerCard key={participant.id} hotDogs={participant.hotdogs} ready={participant.ready} />
+          {!participants.filter((participant) => !isMe(participant.user_id, participant.id)).length && <span className="text-rose-700">Waiting for other players</span> }
+          {participants.filter((participant) => !isMe(participant.user_id, participant.id)).map((participant) => (
+            <OtherPlayerCard key={participant.id} hotDogs={participant.hotdogs} ready={participant.ready} gameStarted={gameStarted} />
           ))}
         </div>
       </section>
@@ -159,7 +186,7 @@ const ShowJoinCodeButton = tw.button`
   text-rose-800
   hover:text-neutral-800
   py-2
-  my-2
+  mt-4
 `
 
 const StartGameButton = tw.button`
