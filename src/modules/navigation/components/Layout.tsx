6f955c74875e6a75ff8cd269c1b223198f98278f
unknown <niklas@demtrees.com>
2022-08-21 16:23:42 +0300

    Add simple leaderboard page

--- src/modules/navigation/components/Layout.tsx
+++ src/modules/navigation/components/Layout.tsx
@@ -1,29 +1,81 @@
 import React from 'react'
-import { Outlet, Link } from "react-router-dom";
-import tw from 'twin.macro'
+import { Outlet, NavLink } from "react-router-dom";
+import tw, {css} from 'twin.macro'
 import BgPattern from '../../../assets/bg-pattern.svg'
+import { useSupabase } from '../../../contexts/SupabaseContext';
 
 
 export default function Layout() {
+  const { client, session } = useSupabase()
+
+  async function handleLogout () {
+    await client.auth.signOut()
+  }
+
+  const activeClass = 'font-black border-b-2 border-red-600'
+
   return (
     <>
       <Background style={{ backgroundImage: `url(${BgPattern})` }} />
-      <Wrapper>
+      <Nav>
+        <NavLinks>
+          <li>
+            <NavLink className={({ isActive }) => isActive ? activeClass : ''} to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink className={({ isActive }) => isActive ? activeClass : ''} to="/leaderboard">Leaderboard</NavLink>
+          </li>
+        </NavLinks>
+        {session && <button onClick={handleLogout}>Logout</button>}
+      </Nav>
+      <Main>
         <Outlet />
-      </Wrapper>
+      </Main>
     </>
   );
 }
 
 const Background = tw.div`
-  absolute
+  fixed
   w-full
   h-screen
+  bottom-0
   bg-no-repeat
-  top-0
-
+  top-24
+  overflow-hidden
 `
-const Wrapper = tw.div`
+
+const Nav = tw.nav`
+  fixed
+  top-0
+  flex
+  justify-between
+  p-4
+  z-20
+  left-0
+  right-0
+  container
+  mx-auto
+  after:block
+  after:absolute
+  after:w-full
+  after:h-full
+  after:opacity-75
+  after:bg-orange-300
+  after:-z-10
+  after:top-0
+  after:left-0
+`
+const NavLinks = tw.ul`
+  flex
+  items-center
+  gap-8
+`
+
+const Main = tw.main`
+  container
+  mx-auto
   relative
+  py-8
   z-10
 `
